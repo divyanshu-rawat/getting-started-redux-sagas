9c0e2eb0f0c26d32fadea91248d9a78369c2a8b2
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-20 14:39:22 +0530

    bug fixed

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -4,11 +4,8 @@ import {put, takeLatest, all} from 'redux-saga/effects';
 
 function *getNews(){
 
-	let data;
-	const json = yield axios('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cee62291e6f04d7e89859dec2c2b4b7e')
-                 .then( (response) => {
-                 	data = response.data.articles;
-                 })
+	const data = yield axios('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cee62291e6f04d7e89859dec2c2b4b7e')
+                 .then( (response) => response.data.articles)
                  .catch(err => err);
 
      	  yield put({ type: "NEWS_RECEIVED", data: data });
